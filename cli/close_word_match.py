4284d46a8856076d4a4cb63f38e75c97e4122346
rohkoder29 <fg99.wd1ny@slmail.me>
2022-09-21 10:35:37 -0300

    Use module name instead of alias

--- cli/close_word_match.py
+++ cli/close_word_match.py
@@ -1,10 +1,10 @@
-import difflib as dfl
+import difflib
 # from difflib import get_close_matches as gcm
 
 words = ['blastula', 'stuff', 'understudy', 'priceless', 'license', 'service']
 
-close_match = dfl.get_close_matches('stu', words)
+close_match = difflib.get_close_matches('stu', words)
 print(close_match)
 
-close_match = dfl.get_close_matches('ice', words)
+close_match = difflib.get_close_matches('ice', words)
 print(close_match)
